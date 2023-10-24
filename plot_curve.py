de8805f5e2a4ef8f364ed55a32367460fc1deed0
Josh Colclough <colcloughj2@cardiff.ac.uk>
2023-10-24 21:34:36 +0100

    Add plot title

--- plot_curve.py
+++ plot_curve.py
@@ -7,7 +7,8 @@ for i in range(len(lf.FAN_CONFIGS)):
     s = np.array([lf.get_speed_from_curve(t, lf.FAN_CONFIGS[i]) for t in T])
     plt.plot(T, s, label=lf.FAN_CONFIG_NAMES[i])
 
-plt.xlabel("Temperature (C)")
+plt.title("liquidfan fan curves.")
+plt.xlabel("Liquid temperature (C)")
 plt.ylabel("Fan speed (from 0 -> 1).")
 plt.legend()
 plt.show()
