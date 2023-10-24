40e57bd7d1b47c0b19acf4e9e696bc1871870609
Josh Colclough <colcloughj2@cardiff.ac.uk>
2023-10-24 21:18:57 +0100

    Add axis labels.

--- plot_curve.py
+++ plot_curve.py
@@ -5,10 +5,10 @@ import numpy as np
 for i in range(len(lf.FAN_CONFIGS)):
     T = np.linspace(0, 100, num=200)
     s = np.array([lf.get_speed_from_curve(t, lf.FAN_CONFIGS[i]) for t in T])
-    print(s)
-
     plt.plot(T, s, label=lf.FAN_CONFIG_NAMES[i])
 
+plt.xlabel("Temperature (C)")
+plt.ylabel("Fan speed (from 0 -> 1).")
 plt.legend()
 plt.show()
 
